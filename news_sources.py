c3d35f6c2e5548a59bd3db9c59368793cb2e6eff
shubhneetkaurriar-crypto <shubhneetkaurriar@gmail.com>
2026-08-06 18:48:59 +0530

    Create news_sources.py

---
+++ news_sources.py
@@ -0,0 +1,29 @@
+RSS_FEEDS = {
+
+    # Official Sources
+    "PIB":
+    "https://pib.gov.in/RssMain.aspx",
+
+    "PRS India":
+    "https://prsindia.org/rss",
+
+    "RBI":
+    "https://www.rbi.org.in/rss/PressReleases.xml",
+
+    # Newspapers
+    "The Hindu National":
+    "https://www.thehindu.com/news/national/feeder/default.rss",
+
+    "The Hindu Sci-Tech":
+    "https://www.thehindu.com/sci-tech/feeder/default.rss",
+
+    "Indian Express":
+    "https://indianexpress.com/section/india/feed/",
+
+    # Environment
+    "Down To Earth":
+    "https://www.downtoearth.org.in/rss",
+
+    "Mongabay India":
+    "https://india.mongabay.com/feed/"
+}
